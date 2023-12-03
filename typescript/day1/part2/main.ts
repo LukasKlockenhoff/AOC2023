749c7e05ba7899087b70d04c0ef5059b46473007
lukasklockenhoff <lukasklockenhoff@icloud.com>
2023-12-03 23:30:47 +0100

    day1 completion

--- typescript/day1/part2/main.ts
+++ typescript/day1/part2/main.ts
@@ -1,5 +1,11 @@
 import * as fs from 'fs';
 
+type NumberInString = {
+    number: number;
+    index: number;
+
+}
+
 function readInput(filename: string): string[] {
     const data = fs.readFileSync(filename, 'utf8');
     return data.split('\n');
@@ -8,90 +14,77 @@ function readInput(filename: string): string[] {
 function getCalibrationCode(input: string): number {;
     const firstNumber = findFirstNumber(input);
     const lastNumber = findLastNumber(input);
-    const inputBeforeFirstNumber = input.slice(0, input.indexOf(firstNumber.toString()));
+    const inputBeforeFirstNumber = input.slice(0, firstNumber.index);
     const firstSpelledOutNumber = getFirstSpelledOutNumber(inputBeforeFirstNumber);
-    const inputAfterLastNumber = input.slice(input.indexOf(lastNumber.toString()) + 1);
+    const inputAfterLastNumber = input.slice(lastNumber.index + 1);
     const lastSpelledOutNumber = getLastSpelledOutNumber(inputAfterLastNumber);
     if (firstSpelledOutNumber === -1 && lastSpelledOutNumber === -1) {
-        return parseInt(firstNumber.toString() + lastNumber.toString());
+        return parseInt(firstNumber.number.toString() + lastNumber.number.toString());
     } else if (firstSpelledOutNumber === -1) {
-        return parseInt(firstNumber.toString() + lastSpelledOutNumber.toString());
+        return parseInt(firstNumber.number.toString() + lastSpelledOutNumber.toString());
     } else if (lastSpelledOutNumber === -1) {
-        return parseInt(firstSpelledOutNumber.toString() + lastNumber.toString());
+        return parseInt(firstSpelledOutNumber.toString() + lastNumber.number.toString());
     }
     return parseInt(firstSpelledOutNumber.toString() + lastSpelledOutNumber.toString());
 }
 
-function findFirstNumber(input: string): number {
+function findFirstNumber(input: string): NumberInString {
     let num = 0;
     for (let i = 0; i < input.length; i++) {
         const currentNumber = parseInt(input[i]);
         if (currentNumber) {
             num += currentNumber;
-            break;
+            return {number: num, index: i};
         }
     }
-    return num;
+    return {number: num, index: -1};
 }
 
-function findLastNumber(input: string): number {
+function findLastNumber(input: string): NumberInString {
     let num = 0;
     for (let i = input.length - 1; i >= 0; i--) {
         const currentNumber = parseInt(input[i]);
         if (currentNumber) {
             num += currentNumber;
-            break;
+            return {number: num, index: i};
         }
     }
-    return num;
+    return {number: num, index: -1};
 }
 
 function getFirstSpelledOutNumber(input: string): number {
-    // returns the index of the spelled out number if it exists, otherwise returns -1
+    let iteratedString = input.slice(0, 3);
     let spelledOutNumbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const spelledOutNumbersInString:string[] = [];
-    for (let i = 0; i < spelledOutNumbers.length; i++) {
-        if (input.includes(spelledOutNumbers[i])) {
-            spelledOutNumbers[i] = input.indexOf(spelledOutNumbers[i]).toString();
+
+    for (let i = 0; i < input.length; i++) {
+        for (let i = 0; i < spelledOutNumbers.length; i++) {
+            if (iteratedString.includes(spelledOutNumbers[i])) {
+                return i + 1;
+            }
         }
-    }
-    let smallestNumber = Infinity;
-    for (let i = 0; i < spelledOutNumbers.length; i++) {
-        if (parseInt(spelledOutNumbers[i]) < smallestNumber) {
-            smallestNumber = parseInt(spelledOutNumbers[i]);
-        }
-    }
-    if (smallestNumber !== Infinity) {
-        return spelledOutNumbers.indexOf(smallestNumber.toString()) + 1;
+        iteratedString = input.slice(0, 3 + i);
     }
     return -1;
 }
 
 function getLastSpelledOutNumber(input: string): number {
-    // returns the index of the spelled out number if it exists, otherwise returns -1
+    let iteratedString = input.slice(input.length - 3);
     let spelledOutNumbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const spelledOutNumbersInString:string[] = [];
-    for (let i = 0; i < spelledOutNumbers.length; i++) {
-        if (input.includes(spelledOutNumbers[i])) {
-            spelledOutNumbers[i] = input.indexOf(spelledOutNumbers[i]).toString();
+
+    for (let i = 0; i < input.length; i++) {
+        for (let i = 0; i < spelledOutNumbers.length; i++) {
+            if (iteratedString.includes(spelledOutNumbers[i])) {
+                return i + 1;
+            }
         }
-    }
-    let largestNumber = -Infinity;
-    for (let i = 0; i < spelledOutNumbers.length; i++) {
-        if (parseInt(spelledOutNumbers[i]) > largestNumber) {
-            largestNumber = parseInt(spelledOutNumbers[i]);
-        }
-    }
-    if (largestNumber !== -Infinity) {
-        return spelledOutNumbers.indexOf(largestNumber.toString()) + 1;
+        iteratedString = input.slice(input.length - 3 - i);
     }
     return -1;
 }
-
-
 function main() {
     const input = readInput('../input.txt');
     let calibrationCode = 0;
+    let counter = 0;
     for (let i = 0; i < input.length; i++) {
         const result = getCalibrationCode(input[i]);
         calibrationCode += result;
